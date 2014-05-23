927d121a6ca4532d8f4de4897bb9fe3a5d034a2d
Chris Haueter <chaueter@pivotallabs.com>
2014-05-23 17:07:04 -0400

    test apps without vendored dependencies can be deployed

--- cf_spec/integration/deploy_a_node_app_spec.rb
+++ cf_spec/integration/deploy_a_node_app_spec.rb
@@ -8,4 +8,15 @@ describe 'deploying a nodejs app' do
       expect(app.homepage_html).to include "Hello, World!"
     end
   end
+
+  it "deploys apps without vendored dependencies", if: Machete::BuildpackMode.online? do
+    app_name = "node_web_app_no_dependencies"
+
+    Dir.exists?("cf_spec/fixtures/#{app_name}/node_modules").should be_false
+
+    Machete.deploy_app(app_name, :nodejs) do |app|
+      expect(app).to be_staged
+      expect(app.homepage_html).to include "Hello, World!"
+    end
+  end
 end

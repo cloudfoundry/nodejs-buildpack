435c3962df4bbac54004f8fddd72f3f11b7a09c6
Chris Haueter <chaueter@pivotallabs.com>
2014-05-23 17:02:57 -0400

    use Procfile instead of startup command for test app

--- cf_spec/integration/deploy_a_node_app_spec.rb
+++ cf_spec/integration/deploy_a_node_app_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 
 describe 'deploying a nodejs app' do
   it "makes the homepage available" do
-    Machete.deploy_app("node_web_app", :nodejs, {
-      cmd: "node server.js"
-    }) do |app|
+    Machete.deploy_app("node_web_app", :nodejs) do |app|
       expect(app).to be_staged
       expect(app.homepage_html).to include "Hello, World!"
     end

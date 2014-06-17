be87f6c4f56d64213788bdd2ed70da805f7fdd6b
Aaron Triantafyllidis and Tim Johnson <pair+atriantafyllidis+buddhistpirate@pivotallabs.com>
2014-06-17 17:32:56 -0400

    Cache mode does not try to reach the internet
    [#73420832]

--- cf_spec/integration/deploy_a_node_app_spec.rb
+++ cf_spec/integration/deploy_a_node_app_spec.rb
@@ -1,22 +1,42 @@
 $: << 'cf_spec'
 require 'spec_helper'
 
-describe 'deploying a nodejs app' do
-  it "makes the homepage available" do
-    Machete.deploy_app("node_web_app") do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "Hello, World!"
+describe 'CF NodeJS Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+
+  context 'with cached buildpack dependencies' do
+    context 'in an offline environment', if: Machete::BuildpackMode.offline? do
+      let(:app_name) { 'node_web_app' }
+
+      specify do
+        expect(app).to be_staged
+        expect(app.homepage_html).to include 'Hello, World!'
+        expect(app).to have_no_internet_traffic
+      end
     end
   end
 
-  it "deploys apps without vendored dependencies", if: Machete::BuildpackMode.online? do
-    app_name = "node_web_app_no_dependencies"
+  context 'without cached buildpack dependencies' do
+    context 'in an online environment', if: Machete::BuildpackMode.online? do
 
-    expect(Dir.exists?("cf_spec/fixtures/#{app_name}/node_modules")).to eql false
+      context 'app has dependencies' do
+        let(:app_name) { 'node_web_app' }
 
-    Machete.deploy_app(app_name) do |app|
-      expect(app).to be_staged
-      expect(app.homepage_html).to include "Hello, World!"
+        specify do
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'Hello, World!'
+        end
+      end
+
+      context 'app has no dependencies' do
+        let(:app_name) { 'node_web_app_no_dependencies' }
+
+        specify do
+          expect(Dir.exists?("cf_spec/fixtures/#{app_name}/node_modules")).to eql false
+          expect(app).to be_staged
+          expect(app.homepage_html).to include 'Hello, World!'
+        end
+      end
     end
   end
 end

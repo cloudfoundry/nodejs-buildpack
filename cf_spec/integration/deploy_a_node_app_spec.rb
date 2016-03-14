ddaf4457799399ede7ab38204eaa75de0b81cf24
JT Archie <jtarchie@gmail.com>
2016-03-14 11:52:55 -0400

    add nodeJS 0.12.12, 4.4.0, 5.8.0 [#115599307]
    Drop 0.12.10, 4.3.2, 5.7.1

--- cf_spec/integration/deploy_a_node_app_spec.rb
+++ cf_spec/integration/deploy_a_node_app_spec.rb
@@ -14,8 +14,7 @@ describe 'CF NodeJS Buildpack' do
 
     it 'resolves to a nodeJS version successfully' do
       expect(app).to be_running
-      expect(app).to_not have_logged 'Downloading and installing node 4.3.0'
-      expect(app).to have_logged /Downloading and installing node 4\.3\.[^0]/
+      expect(app).to have_logged /Downloading and installing node 4\.\d+\.\d+/
 
       browser.visit_path('/')
       expect(browser).to have_body('Hello, World!')

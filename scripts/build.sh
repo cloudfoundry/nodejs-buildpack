6b93663abf556841aef0202bbc87f29ed1a51368
Dave Goddard <dave@goddard.id.au>
2018-01-09 08:51:02 -0400

    Update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-go build -ldflags="-s -w" -o bin/supply nodejs/supply/cli
-go build -ldflags="-s -w" -o bin/finalize nodejs/finalize/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/supply nodejs/supply/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/finalize nodejs/finalize/cli

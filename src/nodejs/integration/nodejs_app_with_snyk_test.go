7f9dcfd2a1153b3ce91b2ddf450c5abeb3dad6ea
AH7 <assaf.he2@gmail.com>
2018-02-12 20:00:37 +0200

    Add support for custom api url and snyk org

--- src/nodejs/integration/nodejs_app_with_snyk_test.go
+++ src/nodejs/integration/nodejs_app_with_snyk_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/cloudfoundry/libbuildpack/cutlass"
 
@@ -11,43 +12,59 @@ import (
 )
 
 var _ = Describe("CF NodeJS Buildpack", func() {
-	var app *cutlass.App
-	var serviceName string
+	var app, sbApp *cutlass.App
+	var sbUrl string
 	RunCf := func(args ...string) error {
 		command := exec.Command("cf", args...)
 		command.Stdout = GinkgoWriter
 		command.Stderr = GinkgoWriter
 		return command.Run()
 	}
-	AfterEach(func() {
-		if app != nil {
-			app.Destroy()
-		}
-		app = nil
 
-		Expect(RunCf("delete-service", "-f", serviceName)).To(Succeed())
+	AfterEach(func() {
+		app = DestroyApp(app)
+
+		command := exec.Command("cf", "purge-service-offering", "-f", "snyk")
+		command.Stdout = GinkgoWriter
+		command.Stderr = GinkgoWriter
+		_ = command.Run()
+
+		command = exec.Command("cf", "delete-service-broker", "-f", "snyk")
+		command.Stdout = GinkgoWriter
+		command.Stderr = GinkgoWriter
+		_ = command.Run()
+
+		sbApp = DestroyApp(sbApp)
 	})
 
 	BeforeEach(func() {
-		app = cutlass.New(filepath.Join(bpDir, "fixtures", "logenv"))
+		sbApp = cutlass.New(filepath.Join(bpDir, "fixtures", "fake_snyk_service_broker"))
+		Expect(sbApp.Push()).To(Succeed())
+		Eventually(func() ([]string, error) { return sbApp.InstanceStates() }, 20*time.Second).Should(Equal([]string{"RUNNING"}))
+
+		var err error
+		sbUrl, err = sbApp.GetUrl("")
+		Expect(err).ToNot(HaveOccurred())
+
+		RunCf("create-service-broker", "snyk", "username", "password", sbUrl, "--space-scoped")
+		RunCf("create-service", "snyk", "public", "snyk")
+
+		app = cutlass.New(filepath.Join(bpDir, "fixtures", "with_snyk"))
 		app.SetEnv("BP_DEBUG", "true")
-
 		PushAppAndConfirm(app)
-
-		serviceName = "snyk-" + cutlass.RandStringRunes(20) + "-service"
 	})
 
-	Context("deploying a NodeJS app with Snyk service", func() {
+	Context("bind NodeJS app with snyk service", func() {
 		BeforeEach(func() {
-			Expect(RunCf("cups", serviceName, "-p", "'{\"SNYK_TOKEN\":\"secrettoken\"}'")).To(Succeed())
-			Expect(RunCf("bind-service", app.Name, serviceName)).To(Succeed())
+			RunCf("bind-service", app.Name, "snyk")
 
-			_ = RunCf("restage", app.Name)
+			app.Stdout.Reset()
+			RunCf("restage", app.Name)
 		})
 
 		It("test if Snyk token was found", func() {
 			Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
-			Expect(app.Stdout.String()).To(ContainSubstring("Snyk token was found."))
+			Expect(app.Stdout.String()).To(ContainSubstring("Snyk token was found"))
 		})
 	})
 })

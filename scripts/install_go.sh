f56df366e3238350e1c252ebe61d80ad03a6b01b
João Pedro Poloni Ponce <jp052@hotmail.com>
2018-03-06 15:44:18 -0300

    ffmpeg

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -28,3 +28,24 @@ if [ ! -f $GoInstallDir/go/bin/go ]; then
   exit 1
 fi
 
+indent() {
+  sed -u 's/^/       /'
+}
+
+echo "-----> Install ffmpeg"
+BUILD_DIR=$1
+VENDOR_DIR="vendor"
+DOWNLOAD_URL="http://flect.github.io/heroku-binaries/libs/ffmpeg.tar.gz"
+
+echo "DOWNLOAD_URL = " $DOWNLOAD_URL | indent
+
+cd $BUILD_DIR
+mkdir -p $VENDOR_DIR
+cd $VENDOR_DIR
+curl -L --silent $DOWNLOAD_URL | tar xz
+
+echo "exporting PATH and LIBRARY_PATH" | indent
+PROFILE_PATH="$BUILD_DIR/.profile.d/ffmpeg.sh"
+mkdir -p $(dirname $PROFILE_PATH)
+echo 'export PATH="$PATH:$HOME/vendor/ffmpeg/bin"' >> $PROFILE_PATH
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/vendor/ffmpeg/lib"' >> $PROFILE_PATH

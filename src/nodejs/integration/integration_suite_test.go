2588381b234b804834aab6bede8718843d7c34b4
Danny Joyce <djoyce@pivotal.io>
2018-05-29 17:56:48 -0400

    Update integration tests to use CF_STACK.
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- src/nodejs/integration/integration_suite_test.go
+++ src/nodejs/integration/integration_suite_test.go
@@ -33,7 +33,7 @@ func init() {
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
 	if buildpackVersion == "" {
-		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack("cflinuxfs2")
+		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack(os.Getenv("CF_STACK"))
 		Expect(err).NotTo(HaveOccurred())
 
 		data, err := json.Marshal(packagedBuildpack)

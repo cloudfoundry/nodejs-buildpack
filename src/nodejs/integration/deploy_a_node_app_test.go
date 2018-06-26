1492b8743cc62c56904791c3b8b05534d1443f40
Tyler Phelan <tphelan@pivotal.io>
2018-06-26 16:04:45 -0400

    Remove node 4 everywhere
    - Node 4 is no longer in the buildpack
    
    [#157283396]

--- src/nodejs/integration/deploy_a_node_app_test.go
+++ src/nodejs/integration/deploy_a_node_app_test.go
@@ -29,7 +29,7 @@ var _ = Describe("CF NodeJS Buildpack", func() {
 			It("resolves to a nodeJS version successfully", func() {
 				PushAppAndConfirm(app)
 
-				Eventually(app.Stdout.String).Should(MatchRegexp("Installing node 4\\.\\d+\\.\\d+"))
+				Eventually(app.Stdout.String).Should(MatchRegexp("Installing node 6\\.\\d+\\.\\d+"))
 				Expect(app.GetBody("/")).To(ContainSubstring("Hello, World!"))
 			})
 		})

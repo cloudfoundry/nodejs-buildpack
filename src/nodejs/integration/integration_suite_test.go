70e31469c5b6bf47bfa4aa7fe54cf40069cc2db4
Leah Hanson <lhanson@pivotal.io>
2018-07-17 14:14:34 -0400

    Update for libbuildpack changes
    - Check that the app actually deploys when running no internet/proxy tests.
    
    [#158774977]
    
    Signed-off-by: Danny Joyce <djoyce@pivotal.io>

--- src/nodejs/integration/integration_suite_test.go
+++ src/nodejs/integration/integration_suite_test.go
@@ -126,13 +126,14 @@ func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
 			Expect(err).To(BeNil())
 			defer os.Remove(bpFile)
 
-			traffic, err := cutlass.InternetTraffic(
+			traffic, built, err := cutlass.InternetTraffic(
 				bpDir,
 				filepath.Join("fixtures", fixtureName),
 				bpFile,
 				[]string{"HTTP_PROXY=" + proxy.URL, "HTTPS_PROXY=" + proxy.URL},
 			)
 			Expect(err).To(BeNil())
+			Expect(built).To(BeTrue())
 
 			destUrl, err := url.Parse(proxy.URL)
 			Expect(err).To(BeNil())
@@ -156,13 +157,14 @@ func AssertNoInternetTraffic(fixtureName string) {
 		Expect(err).To(BeNil())
 		defer os.Remove(bpFile)
 
-		traffic, err := cutlass.InternetTraffic(
+		traffic, built, err := cutlass.InternetTraffic(
 			bpDir,
 			filepath.Join("fixtures", fixtureName),
 			bpFile,
 			[]string{},
 		)
 		Expect(err).To(BeNil())
+		Expect(built).To(BeTrue())
 		Expect(traffic).To(BeEmpty())
 	})
 }

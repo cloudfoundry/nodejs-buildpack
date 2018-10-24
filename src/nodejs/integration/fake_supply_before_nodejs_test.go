07afde3803ace36b99066d9e58b1c7dc06fab998
Danny Joyce <djoyce@pivotal.io>
2018-10-24 15:50:32 -0400

    Add multi-buildpack tests [#149434941]
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

---
+++ src/nodejs/integration/fake_supply_before_nodejs_test.go
@@ -0,0 +1,41 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("running supply buildpacks before the nodejs buildpack", func() {
+	var app *cutlass.App
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	Context("the app is pushed once", func() {
+		BeforeEach(func() {
+			if ok, err := cutlass.ApiGreaterThan("2.65.1"); err != nil || !ok {
+				Skip("API version does not have multi-buildpack support")
+			}
+
+			app = cutlass.New(filepath.Join(bpDir, "fixtures", "fake_supply_nodejs_app"))
+			app.Buildpacks = []string{
+				"https://github.com/cloudfoundry/dotnet-core-buildpack#develop",
+				"nodejs_buildpack",
+			}
+			app.Disk = "1G"
+		})
+
+		It("finds the supplied dependency in the runtime container", func() {
+			PushAppAndConfirm(app)
+			Expect(app.Stdout.String()).To(ContainSubstring("Supplying Dotnet Core"))
+			Expect(app.GetBody("/")).To(MatchRegexp(`dotnet: \d+\.\d+\.\d+`))
+		})
+	})
+})

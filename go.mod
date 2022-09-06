6ee14ceb201fb23f4879c7e030c6682d2b867e3f
Ryan Moran <rmoran@vmware.com>
2022-09-06 15:53:26 -0700

    Upgrades switchblade to 0.1.1

--- go.mod
+++ go.mod
@@ -4,12 +4,11 @@ require (
 	github.com/Dynatrace/libbuildpack-dynatrace v1.5.0
 	github.com/Masterminds/semver v1.5.0
 	github.com/cloudfoundry/libbuildpack v0.0.0-20220829111556-28a205221ec0
-	github.com/cloudfoundry/switchblade v0.1.0
+	github.com/cloudfoundry/switchblade v0.1.1
 	github.com/elazarl/goproxy v0.0.0-20220529153421-8ea89ba92021 // indirect
 	github.com/golang/mock v1.6.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.20.2
-	github.com/paketo-buildpacks/packit/v2 v2.4.2 // indirect
 	github.com/sclevine/spec v1.4.0
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
--- vendor/github.com/cloudfoundry/switchblade/internal/cloudfoundry/initialize.go
+++ vendor/github.com/cloudfoundry/switchblade/internal/cloudfoundry/initialize.go
@@ -2,7 +2,10 @@ package cloudfoundry
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"strconv"
 
 	"github.com/paketo-buildpacks/packit/v2/pexec"
 )
@@ -28,17 +31,41 @@ func (i Initialize) Run(buildpacks []Buildpack) error {
 	logs := bytes.NewBuffer(nil)
 
 	for _, buildpack := range buildpacks {
+		position := "1000"
+
+		buffer := bytes.NewBuffer(nil)
 		err := i.cli.Execute(pexec.Execution{
-			Args:   []string{"update-buildpack", buildpack.Name, "-p", buildpack.URI},
-			Stdout: logs,
+			Args:   []string{"curl", fmt.Sprintf("/v3/buildpacks?names=%s", buildpack.Name)},
+			Stdout: io.MultiWriter(buffer, logs),
 			Stderr: logs,
 		})
 		if err == nil {
-			continue
+			var payload struct {
+				Resources []struct {
+					Position int `json:"position"`
+				} `json:"resources"`
+			}
+			err = json.NewDecoder(buffer).Decode(&payload)
+			if err != nil {
+				return fmt.Errorf("failed to parse buildpacks: %w", err)
+			}
+
+			if len(payload.Resources) > 0 {
+				position = strconv.Itoa(payload.Resources[0].Position)
+			}
+
+			err = i.cli.Execute(pexec.Execution{
+				Args:   []string{"delete-buildpack", "-f", buildpack.Name},
+				Stdout: logs,
+				Stderr: logs,
+			})
+			if err != nil {
+				return fmt.Errorf("failed to delete buildpack: %s\n\nOutput:\n%s", err, logs)
+			}
 		}
 
 		err = i.cli.Execute(pexec.Execution{
-			Args:   []string{"create-buildpack", buildpack.Name, buildpack.URI, "1000"},
+			Args:   []string{"create-buildpack", buildpack.Name, buildpack.URI, position},
 			Stdout: logs,
 			Stderr: logs,
 		})

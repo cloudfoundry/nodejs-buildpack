4374e30d0e29e7ad70f33e5fded845840208b2d5
ivanovac <ivanovac@gmail.com>
2025-12-10 16:48:39 +0200

    revert meanless change

--- src/nodejs/integration/override_test.go
+++ src/nodejs/integration/override_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cloudfoundry/switchblade"
 	"github.com/sclevine/spec"
 
+	. "github.com/cloudfoundry/switchblade/matchers"
 	. "github.com/onsi/gomega"
 )
 
@@ -37,15 +38,13 @@ func testOverride(platform switchblade.Platform, fixtures string) func(*testing.
 				).
 				Execute(name, filepath.Join(fixtures, "simple"))
 			Expect(err).To(HaveOccurred())
-			Expect(err).To(MatchError(ContainSubstring("App staging failed")))
 
-			// Switchblade now automatically captures staging logs in CF API v3
-			// by fetching them using 'cf logs --recent' when staging fails
-			logsStr := logs.String()
-			Expect(logsStr).To(ContainSubstring("-----> OverrideYML Buildpack"))
-			Expect(logsStr).To(ContainSubstring("-----> Installing node"))
-			Expect(logsStr).To(MatchRegexp(`Copy .*/node.tgz`))
-			Expect(logsStr).To(ContainSubstring("Unable to install node: dependency sha256 mismatch: expected sha256 062d906c87839d03b243e2821e10653c89b4c92878bfe2bf995dec231e117bfc, actual sha256 b56b58ac21f9f42d032e1e4b8bf8b8823e69af5411caa15aee2b140bc756962f"))
+			Expect(logs.String()).To(SatisfyAll(
+				ContainLines(ContainSubstring("-----> OverrideYML Buildpack")),
+				ContainLines(ContainSubstring("-----> Installing node")),
+				ContainLines(MatchRegexp("Copy .*/node.tgz")),
+				ContainLines(ContainSubstring("Unable to install node: dependency sha256 mismatch: expected sha256 062d906c87839d03b243e2821e10653c89b4c92878bfe2bf995dec231e117bfc, actual sha256 b56b58ac21f9f42d032e1e4b8bf8b8823e69af5411caa15aee2b140bc756962f")),
+			))
 		})
 	}
 }

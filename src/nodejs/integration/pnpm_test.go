868498307c1313964d94f7f54bc62207105bf65e
Nico Jensch <github@dr460nf1r3.org>
2026-01-29 11:44:23 +0100

    Add pnpm support

---
+++ src/nodejs/integration/pnpm_test.go
@@ -0,0 +1,82 @@
+package integration_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/cloudfoundry/switchblade"
+	"github.com/sclevine/spec"
+
+	. "github.com/cloudfoundry/switchblade/matchers"
+	. "github.com/onsi/gomega"
+)
+
+func testPNPM(platform switchblade.Platform, fixtures string) func(*testing.T, spec.G, spec.S) {
+	return func(t *testing.T, context spec.G, it spec.S) {
+		var (
+			Expect     = NewWithT(t).Expect
+			Eventually = NewWithT(t).Eventually
+
+			name   string
+			source string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = switchblade.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+
+			source, err = switchblade.Source(filepath.Join(fixtures, "pnpm", "simple"))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(platform.Delete.Execute(name)).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
+		})
+
+		it("successfully deploys and vendors the dependencies via pnpm", func() {
+			deployment, logs, err := platform.Deploy.
+				WithEnv(map[string]string{"BP_DEBUG": "true"}).
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(filepath.Join(source, "node_modules")).ToNot(BeADirectory())
+
+			Expect(logs).To(ContainLines(
+				ContainSubstring("Installing node modules (pnpm-lock.yaml)"),
+			))
+			
+			// Verify store directory is used
+			Expect(logs).To(ContainLines(
+				ContainSubstring("Using pnpm store directory:"),
+			))
+
+			Eventually(deployment).Should(Serve(ContainSubstring("Hello, World!")))
+		})
+
+		context("deploying a Node.js app that uses pnpm workspaces", func() {
+			it.Before(func() {
+				var err error
+				source, err = switchblade.Source(filepath.Join(fixtures, "pnpm", "workspaces"))
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			it("successfully deploys and vendors the dependencies via pnpm workspaces", func() {
+				deployment, logs, err := platform.Deploy.
+					WithEnv(map[string]string{"BP_DEBUG": "true"}).
+					Execute(name, source)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(filepath.Join(source, "node_modules")).ToNot(BeADirectory())
+				
+				Expect(logs).To(ContainLines(
+					ContainSubstring("Installing node modules (pnpm-lock.yaml)"),
+				))
+
+				Eventually(deployment).Should(Serve(ContainSubstring("Hello from Workspace! Hello from sample-lib")))
+			})
+		})
+	}
+}
---
+++ src/nodejs/pnpm/pnpm_test.go
@@ -0,0 +1,135 @@
+package pnpm_test
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/cloudfoundry/nodejs-buildpack/src/nodejs/pnpm"
+
+	"github.com/cloudfoundry/libbuildpack"
+	"github.com/cloudfoundry/libbuildpack/ansicleaner"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+//go:generate mockgen -source=pnpm.go --destination=mocks_test.go --package=pnpm_test
+
+var _ = Describe("PNPM", func() {
+	var (
+		err         error
+		buildDir    string
+		cacheDir    string
+		p           *pnpm.PNPM
+		logger      *libbuildpack.Logger
+		buffer      *bytes.Buffer
+		mockCtrl    *gomock.Controller
+		mockCommand *MockCommand
+	)
+
+	BeforeEach(func() {
+		buildDir, err = os.MkdirTemp("", "nodejs-buildpack.build.")
+		Expect(err).NotTo(HaveOccurred())
+
+		cacheDir, err = os.MkdirTemp("", "nodejs-buildpack.cache.")
+		Expect(err).NotTo(HaveOccurred())
+
+		buffer = new(bytes.Buffer)
+
+		logger = libbuildpack.NewLogger(ansicleaner.New(buffer))
+
+		mockCtrl = gomock.NewController(GinkgoT())
+		mockCommand = NewMockCommand(mockCtrl)
+
+		p = &pnpm.PNPM{
+			Log:     logger,
+			Command: mockCommand,
+		}
+	})
+
+	AfterEach(func() {
+		mockCtrl.Finish()
+
+		err = os.RemoveAll(buildDir)
+		Expect(err).To(BeNil())
+		os.RemoveAll(cacheDir)
+	})
+
+	Describe("Build", func() {
+		var oldNodeHome string
+		var pnpmConfig map[string]string
+		var pnpmInstallArgs []string
+
+		AfterEach(func() {
+			Expect(os.Setenv("NODE_HOME", oldNodeHome)).To(Succeed())
+		})
+		BeforeEach(func() {
+			oldNodeHome = os.Getenv("NODE_HOME")
+			Expect(os.Setenv("NODE_HOME", "test_node_home")).To(Succeed())
+
+			pnpmConfig = map[string]string{}
+			mockCommand.EXPECT().Execute(gomock.Any(), gomock.Any(), gomock.Any(), "pnpm", gomock.Any()).DoAndReturn(func(dir string, stdout, stderr io.Writer, program string, args ...string) error {
+				switch args[0] {
+				case "config":
+					Expect(args[0:2]).To(Equal([]string{"config", "set"}))
+					pnpmConfig[args[2]] = args[3]
+				default:
+					pnpmInstallArgs = append([]string{"pnpm"}, args...)
+				}
+				Expect(dir).To(Equal(buildDir))
+				return nil
+			}).AnyTimes()
+		})
+
+		It("runs pnpm config and install", func() {
+			Expect(p.Build(buildDir, cacheDir)).To(Succeed())
+
+			Expect(buffer.String()).To(ContainSubstring("Installing node modules (pnpm-lock.yaml)"))
+			Expect(buffer.String()).To(ContainSubstring("Using pnpm store directory: " + filepath.Join(cacheDir, ".pnpm-store")))
+
+			Expect(pnpmConfig).To(Equal(map[string]string{
+				"store-dir": filepath.Join(cacheDir, ".pnpm-store"),
+			}))
+
+			Expect(pnpmInstallArgs).To(Equal([]string{
+				"pnpm", "install",
+				"--frozen-lockfile",
+			}))
+		})
+
+		Context("when NPM_CONFIG_PRODUCTION is true", func() {
+			BeforeEach(func() {
+				Expect(os.Setenv("NPM_CONFIG_PRODUCTION", "true")).To(Succeed())
+			})
+
+			AfterEach(func() {
+				Expect(os.Unsetenv("NPM_CONFIG_PRODUCTION")).To(Succeed())
+			})
+
+			It("runs pnpm install with --prod", func() {
+				Expect(p.Build(buildDir, cacheDir)).To(Succeed())
+
+				Expect(buffer.String()).To(ContainSubstring("NPM_CONFIG_PRODUCTION is true, installing only production dependencies"))
+				Expect(pnpmInstallArgs).To(ContainElement("--prod"))
+			})
+		})
+
+		Context("when a vendored pnpm store exists", func() {
+			BeforeEach(func() {
+				Expect(os.Mkdir(filepath.Join(buildDir, ".pnpm-store"), 0755)).To(Succeed())
+			})
+
+			It("runs pnpm install with --offline and uses the vendored store", func() {
+				Expect(p.Build(buildDir, cacheDir)).To(Succeed())
+
+				Expect(buffer.String()).To(ContainSubstring("Found vendored pnpm store"))
+				Expect(buffer.String()).To(ContainSubstring("Running pnpm in offline mode"))
+				
+				Expect(pnpmConfig).To(HaveKeyWithValue("store-dir", filepath.Join(buildDir, ".pnpm-store")))
+				Expect(pnpmInstallArgs).To(ContainElement("--offline"))
+			})
+		})
+	})
+})
